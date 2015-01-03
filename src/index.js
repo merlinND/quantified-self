9be2791a22f5dd5cb66d3a44828d0852951173e3
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-01-03 11:45:48 +0100

    Reporter: `groupBy`

--- src/index.js
+++ src/index.js
@@ -9,19 +9,24 @@ var reporterData = require('../data/' + reporterFilename);
 // ----- Filtering
 // var start = new Date('2014-07-01 00:00');
 // var end = new Date('2014-011-01 00:00');
-var snapshots = reporterData.snapshots.filter(Reporter.filters.byMonth(6));
+// var snapshots = reporterData.snapshots.filter(Reporter.filters.byMonth(6));
+
+// ----- Grouping
+var grouped = Reporter.groupBy.date(reporterData.snapshots);
+var snapshots = grouped.asList();
 
 // ----- Display
-console.log(snapshots.length);
+console.log(snapshots.map(function(group) { return group.length; }));
 // var logger = function(s) {
 //   console.log(new Date(s.date));
 // };
 // snapshots.map(logger);
 
 // ----- Basic analaysis
-var data = {
-  snapshots: snapshots,
-  questions: reporterData.questions
-};
-var stats = Reporter.getStats(data);
-Reporter.printMainTokens(stats.questions, 10);
+// TODO: support grouped data
+// var data = {
+//   snapshots: snapshots,
+//   questions: reporterData.questions
+// };
+// var stats = Reporter.getStats(data);
+// Reporter.printMainTokens(stats.questions, 10);
--- src/reporter.js
+++ src/reporter.js
@@ -222,6 +222,42 @@ filters.happenedAfter = function(date) {
   return filters.byDate(date);
 };
 
+
+/**
+ * A collection of grouping functions
+ */
+var groupBy = {
+  /**
+   * @param `snapshots` Array of elements
+   * @param `criterion` Function Element => Key
+   */
+  criterion: function(snapshots, criterion) {
+    var Groups = function(snapshots) {
+      var self = this;
+      snapshots.forEach(function(element) {
+        var group = criterion(element);
+        self[group] = self[group] || [];
+        self[group].push(element);
+      });
+    };
+
+    /** Utility function */
+    Groups.prototype.asList = function() {
+      var self = this;
+      return Object.keys(self).map(function(key) { return self[key]; });
+    };
+
+    return new Groups(snapshots);
+  },
+
+  date: function(snapshots) {
+    return groupBy.criterion(snapshots, function(snapshot) {
+      var d = new Date(snapshot.date);
+      return new Date(d.getFullYear(), d.getMonth(), d.getDate());
+    });
+  },
+};
+
 /**
  * Get simple stats (number of entries, etc)
  */
@@ -280,5 +316,6 @@ module.exports = {
   getStats: getStats,
   printMainTokens: printMainTokens,
   filters: filters,
+  groupBy: groupBy,
   reasons: reasons
 };
